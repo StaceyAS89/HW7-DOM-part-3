77c3dc9409500de5bbc7aa5051a3ca35132d354a
StaceyAS89 <anastasiiaforapple@gmail.com>
2021-11-24 15:49:13 +0200

    HW7 version1.1

--- main.js
+++ main.js
@@ -6,29 +6,25 @@ let li = document.createElement('li');
 
 
 
-button.addEventListener('click', function(event){
-   let li = document.createElement('li');
+button.addEventListener('click', function (event) {
    const btnRemove = document.createElement('button')
 
-   btnRemove.innerText='remove';
+   btnRemove.innerText = 'remove';
    btnRemove.classList.add('btn_remove_task');
    ul.appendChild(li);
    li.classList.add('task_is_not_done');
    li.textContent = input.value;
-  
+
    input.value = '';
    li.appendChild(btnRemove);
 })
- ul.addEventListener('click', function(event) {
-     
-    if (event.target.tagName ==="LI"){
-     event.target.classList.toggle('task_is_done') }
-     if (event.target.tagName ==="BUTTON") {
-         console.log("jndnjdnc")
+ul.addEventListener('click', function (event) {
+
+   if (event.target.tagName === "LI") {
+      event.target.classList.toggle('task_is_done')
+   }
+   if (event.target.tagName === "BUTTON") {
       event.target.closest('li').remove();
-     }
-   
- })
-    
- 
-   
+   }
+
+})
